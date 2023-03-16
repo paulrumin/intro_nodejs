8220b4263bf5509787c5836d0bcba96cc71459d0
paulrumin <pasha@jedo.localdomain>
2023-03-16 08:23:19 -0700

    Server Lessons

--- node_basic/app.js
+++ node_basic/app.js
@@ -1,3 +1,10 @@
-var utilities = require('./utilities')
+var http = require('http')
 
-console.log( utilities.sayHello());
+http.createServer(function( req, res ){
+    res.writeHead(200, {'Content-Type': 'text-plain'});
+    res.end('Hello World');
+}).listen(3000,'127.0.0.1');
+
+console.log('Server listening at 127.0.0.1:3000');
+
+
