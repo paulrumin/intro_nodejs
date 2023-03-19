6d15e0467f0f3cb68dd9d505e011aa9cbb2f5e65
paulrumin <pasha@jedo.localdomain>
2023-03-19 05:40:08 -0700

    Express Part 2

--- node-express-full/app.js
+++ node-express-full/app.js
@@ -13,6 +13,10 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.get('/', function(req,res){
+    res.render('index');
+});
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
